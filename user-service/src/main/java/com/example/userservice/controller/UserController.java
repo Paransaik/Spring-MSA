6828464dd816d0d4fe20d55a6acd3ef6c28e5a3a
TaeyeongJeong <jty419@naver.com>
2022-12-05 09:11:49 +0900

    Add:: 1205 TY Initialize Project

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+    private Environment env;
+
+    @Autowired
+    private Greeting greeting;
+
+    @Autowired
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Servcie";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        // Case 1:: message 가져오기
+        // return env.getProperty("greeting.message");
+        // Case 2:: message 가져오기
+        return greeting.getMessage();
+    }
+}
